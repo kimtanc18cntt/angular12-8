d2fbafb51638f067ef0c6316050944d45e887801
kimtanc18cntt <lekimtan0610@gmai.com>
2022-08-10 17:30:28 +0700

    10/8

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,10 +3,9 @@ import { RouterModule, Routes } from '@angular/router';
 import {ProductComponent} from './component/product/product.component'
 import {PagenotcomponentComponent} from './component/pagenotcomponent/pagenotcomponent.component'
 import {CartComponent} from './component/cart/cart.component';
-import {HomeComponent} from './component/home/home.component'
 import {RegisterComponent} from './component/register/register.component'
-import {DialogComponent} from './component/dialog/dialog.component';
-import {CoinListComponent} from './component/coin-list/coin-list.component'
+import {CoinListComponent} from './component/coin-list/coin-list.component';
+import {CoinDetailComponent} from './component/coin-detail/coin-detail.component';
 
 const routes: Routes = [ 
 { path: 'home', component:  CoinListComponent},
@@ -14,8 +13,8 @@ const routes: Routes = [
 { path: 'cart', component: CartComponent },
 { path: 'addd', component: ProductComponent },
 { path: 'register', component: RegisterComponent },
-{ path: 'dialog', component: DialogComponent },
 { path: '', redirectTo: '/home', pathMatch: 'full' },
+{path:'coin-detail/:id', component: CoinDetailComponent},
 { path: '**', component: PagenotcomponentComponent}];
 
 @NgModule({
